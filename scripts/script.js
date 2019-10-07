04f0d5bab8757c135e38ac4399b6d631d72d34aa
Holodnitscky Sergei <47819799+mydissi@users.noreply.github.com>
2019-10-08 00:33:10 +0500

    Added everything

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+var clock = document.getElementById('clock');
+var color = document.getElementById('color');
+
+
+function weirdClock()
+{
+
+	var time = new Date();
+	var h = time.getHours().toString();
+	var m = time.getMinutes().toString();
+	var s = time.getSeconds().toString();
+
+	if(h.length < 2)
+	{
+		h = '0' + h;
+	}
+	if(m.length < 2)
+	{
+		m = '0' + m;
+	}
+	if(s.length < 2)
+	{
+		s = '0' + s;
+	}
+
+	var clockString = h + ':' + m + ':' + s;
+	var colorString = '#' + h + m + s;
+
+	clock.textContent = clockString;
+	color.textContent = colorString;
+
+	document.body.style.background = colorString;
+
+}
+
+function moscowTime()
+{
+	alert("Retry!");
+}
+
+
+weirdClock();
+setInterval(weirdClock,1000);
